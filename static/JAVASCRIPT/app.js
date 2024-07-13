604925fe2999c5cb7bad333182847dad6670cea1
Muhammad Qasim <malikqasim20051@gmail.com>
2024-07-13 17:24:04 +0500

    OUR FEATURED CARS SECTION COMPLETED

--- static/JAVASCRIPT/app.js
+++ static/JAVASCRIPT/app.js
@@ -121,3 +121,26 @@ $('.counter').counterUp({
     time: 2000
   });
   $('h3').addClass('animated fadeIn');
+
+
+
+// 6) Card Slider
+
+
+$('.owl-carousel-1').owlCarousel({
+  loop: false,
+  nav:false,
+  margin:20,
+  autoplay: false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
