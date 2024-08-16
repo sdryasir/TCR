7649b6a3c5e911bedbd60e6bf6db211c7a75f96c
Daniyal Ahmed Sethi <daniyalahmedsethi@gmail.com>
2024-08-16 20:50:26 +0500

    1

--- The_Car_Rental/urls.py
+++ The_Car_Rental/urls.py
@@ -15,20 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
-from django.views.static import serve
+
 
 
 
 
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', views.homePage, name='home'),
     path('about/', views.aboutPage, name='about'),
