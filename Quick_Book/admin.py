6e7785a6ac5a3d932ce2657eb6d498d69c07812d
Daniyal ahmed sethi <daniyalahmedsethi@gmail.com>
2024-09-09 15:03:37 +0500

    66

--- Quick_Book/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Quick_Book
-class Quick_Book_Admin(admin.ModelAdmin):
-    list_display=['name','car','phone', 'email']
-
-admin.site.register(Quick_Book, Quick_Book_Admin)
