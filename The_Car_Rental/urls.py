13a2d614bb4a52720a4b0577d078b5479805e2d3
Daniyal Ahmed Sethi <daniyalahmedsethi@gmail.com>
2024-09-10 13:23:36 +0500

    888

--- The_Car_Rental/urls.py
+++ The_Car_Rental/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('blog/', views.blogPage, name='blog'),
     path('single-post/<id>', views.Single_postPage, name='single_post'),
     path('checkout/', views.checkout_view, name='checkout'),
-    path('process_checkout/', views.process_checkout, name='process_checkout'),
+    path('process_checkout/', views.process_checkout, name='order_confirmation'),
     path('payment-success/', views.successPage, name='success'),
     path('payment-failed/', views.cancelPage, name='cancel'),
 
